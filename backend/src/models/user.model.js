4e718d0fd612be0961e857cb90d3a88c98c5ec17
Rohit-Majee <rohitmajee665@gmail.com>
2025-08-24 22:02:26 +0530

    fixed mongoose error

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, unique: true, required: true },
     name: { type: String, required: true },
     image: { type: String, required: true },
-    clerkID: { type: String, unique: true, required: true },
+    clerkId: { type: String, unique: true, required: true },
   },
   { timestamps: true }
 );
